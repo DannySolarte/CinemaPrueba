809aca44671fe669e25c5f8d320ee972757930c4
Daniel Naranjo <arq.danielnaranjo@hotmail.com>
2023-11-02 21:51:10 -0500

    nuevo

--- src/components/Principal-Img.js
+++ src/components/Principal-Img.js
@@ -2,8 +2,10 @@ const principalImg = () => {
     return (
         <div className="container-imgPrincipal">
           <div className="img-body">
-           <a><h1 className="titulo-imgPrincipal">Saw X</h1></a> <br></br>
-           <button className="button-reserva"><h2>Reservar</h2></button>
+           <a><h1 className="titulo-imgPrincipal">Saw X</h1>
+           <h2>El juego del miedo</h2>
+           </a> <br></br>
+           <button className="button-reserva"><h2>Reserva</h2></button>
           </div>
         </div>
     )
