3ed76df2f3ea5a896ff3c841fcb63d0810b6bcc3
Daniel Naranjo <arq.danielnaranjo@hotmail.com>
2023-11-03 15:38:37 -0500

    texto

--- src/components/Principal-Img.js
+++ src/components/Principal-Img.js
@@ -2,10 +2,11 @@ const principalImg = () => {
     return (
         <div className="container-imgPrincipal">
           <div className="img-body">
-           <a><h1 className="titulo-imgPrincipal">Saw X</h1>
-           <h2>El juego del miedo</h2>
-           </a> <br></br>
-           <a href="https://www.cinecolombia.com/medellin/peliculas/moana"><button className="button-reserva"><h2>Reserva</h2></button></a>
+           <span className="boxText">
+            <a><h1 className="titulo-imgPrincipal">SAW X</h1></a>
+            <span><h1 className="esloganTitulo-Principal">El juego del miedo</h1></span>
+            <span href="https://www.cinecolombia.com/medellin/peliculas/moana"><button className="button-reserva"><h2>Reserva</h2></button></span>
+           </span> <br></br>
           </div>
         </div>
     )
